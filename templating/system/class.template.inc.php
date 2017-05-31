cc61ae6737cf124bc0324e7b670a1d9369449f05
abhinavGirish <abhinav.girish97@gmail.com>
2017-05-31 14:52:08 -0400

    adding initial template

---
+++ templating/system/class.template.inc.php
@@ -0,0 +1,21 @@
+<?php 
+  
+/** * A templating engine * * PHP version 5 * * LICENSE: This source file is subject to the MIT License, available at * http://www.opensource.org/licenses/mit-license.html * * @author Jason 
+Lengstorf <jason.lengstorf@copterlabs.com> * @copyright 2010 Copter Labs * @license http://www.opensource.org/licenses/mit-license.html MIT License */ 
+  class Template { 
+  
+ //TODO: Define a class property to store the template 
+  
+ //TODO: Define a class property to store the entries 
+  
+ //TODO: Write a public method to output the result of the templating engine 
+  
+ //TODO: Write a private method to load the template 
+  
+ //TODO: Write a private method to parse the template 
+  
+ //TODO: Write a static method to replace the template tags with entry data 
+  
+ //TODO: Write a private currying function to facilitate tag replacement 
+  
+}
